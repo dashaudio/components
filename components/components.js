53597e2548fb949d90677c5fc04d8ab2a0f20443
Dan Halliday <dan@danhalliday.com>
2016-07-08 12:35:08 +0100

    New icon page, refreshed colour palette

--- components/components.js
+++ components/components.js
@@ -21,3 +21,9 @@ export { DashAnalyticsSummary } from './analytics/summary/summary'
 export { DashAnalyticsSummaryPlays } from './analytics/summary/plays/plays'
 export { DashAnalyticsSummaryListeners } from './analytics/summary/listeners/listeners'
 export { DashAnalyticsSummaryInstalls } from './analytics/summary/installs/installs'
+
+window.addEventListener('WebComponentsReady', () => {
+  Array.from(document.querySelectorAll('.dash-unresolved')).forEach((item) => {
+    item.className = item.className.replace('dash-unresolved', 'dash-resolved')
+  })
+})
